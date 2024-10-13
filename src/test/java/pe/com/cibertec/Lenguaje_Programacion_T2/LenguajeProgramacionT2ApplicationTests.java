d6760d1914fe8cca5fda23166551dd1a5ef484f5
Malena Vera <luis22vera03@gmail.com>
2024-10-12 22:51:08 -0500

    Estructura inicial del proyecto

---
+++ src/test/java/pe/com/cibertec/Lenguaje_Programacion_T2/LenguajeProgramacionT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.Lenguaje_Programacion_T2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LenguajeProgramacionT2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
